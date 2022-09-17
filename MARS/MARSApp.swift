780727a885b2214cf979ea948198e96de44a973e
Murughan8 <rajamurughan85@gmail.com>
2022-09-17 12:50:08 +0400

    Initial Commit

---
+++ MARS/MARSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MARSApp.swift
+//  MARS
+//
+//  Created by Kalyani on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MARSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
